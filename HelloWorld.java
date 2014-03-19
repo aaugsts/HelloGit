e02fe22a3cb4b0cdc2f80fbd6fdb327700fba050
aaugsts <gy12aa@leeds.ac.uk>
2014-03-19 14:30:22 +0000

    Start

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package E.MSc_GIS_material.SEMESTER2.Programming.pgiaas.hellogit;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+	
+		System.out.println("Hi");
+		
+	}
+	
+	public static void main (String args[]) {
+	
+		new HelloWorld();
+		
+	}
+	
+}
